41300013e598ae4045a83b50c0d9dfad90265481
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-07-17 04:10:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray (array, element) {
-  array = [...chocolateBars, element]
+  array = [...chocolateBars, element];
   return array;
 }
 
