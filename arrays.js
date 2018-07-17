636f1114efbd4af45cffd39083fc8c661fe275fe
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-07-17 06:33:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
 }
 
 function removeElementFromBeginningOfArray (array) {
-  array.slice();
+  array = array.slice();
   return array;
 }
 
